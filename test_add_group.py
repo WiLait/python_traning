4f5d85f676a6423ec6a39bca2f6cd2d9dd36786d
WiLait <wilait.xaxa@gmail.com>
2025-02-18 14:45:24 +0600

    Puchenko

--- test_add_group.py
+++ test_add_group.py
@@ -2,15 +2,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.ie.webdriver import WebDriver
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
 
-class TestAddGroup(unittest.TestCase):
+
+
+class test_add_group(unittest.TestCase):
     def setUp(self):
-        self.dw = webdriver.Firefox()
-        self.dw.implicitly_wait(30)
+        self.dw = WebDriver()
+        self.dw.implicitly_wait(60)
     
     def test_add_group(self):
         dw = self.dw
