ce3da048143acd1a786e5b114b3efe57d423e217
WiLait <wilait.xaxa@gmail.com>
2025-03-03 14:43:00 +0600

    Puchenko

--- test_add_group.py
+++ test_add_group.py
@@ -1,24 +1,12 @@
 # -*- coding: utf-8 -*-
 # -*- coding: utf-8 -*-
-from platformdirs import AppDirs
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
 import unittest
 import pytest
-from urllib3 import request
-
-from application import Application
 from group import Group
 from application import Application
 
 @pytest.fixture
-def app():
+def app(request):
     fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
