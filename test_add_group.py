7e445becc2fdda5268a136c9972dc862c26c084b
WiLait <wilait.xaxa@gmail.com>
2025-03-03 14:32:13 +0600

    Puchenko

--- test_add_group.py
+++ test_add_group.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # -*- coding: utf-8 -*-
+from platformdirs import AppDirs
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -8,91 +9,29 @@ from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
+import pytest
+from urllib3 import request
+
+from application import Application
 from group import Group
+from application import Application
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
+@pytest.fixture
+def app():
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
+def test_add_group(app):
+        app.login("admin", "secret")
+        app.create_group(Group(name="sai", header="ydfghd", footer="dhryftd"))
+        app.logout()
 
-class TestAddGroup(unittest.TestCase):
-    def setUp(self):
-        self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
-
-    def test_add_group(self):
-        wd = self.wd
-        self.login(wd, "admin", "secret")
-        self.create_group(wd, Group(name="sai", header="ydfghd", footer="dhryftd"))
-        self.logout(wd)
-
-    def test_add_empty_group(self):
-        wd = self.wd
-        self.login(wd, "admin", "secret")
-        self.create_group(wd, Group(name="", header="", footer=""))
-        self.logout(wd)
-
-    def logout(self, wd):
-        wd.find_element(By.LINK_TEXT, "Logout").click()
-
-    def return_to_group_page(self, wd):
-        wd.find_element(By.LINK_TEXT, "group page").click()
-
-    def create_group(self, wd, group):
-        self.open_group_page(wd)
-        # init group creation
-        wd.find_element(By.NAME, "new").click()
-        # fill group firm
-        wd.find_element(By.NAME, "group_name").click()
-        wd.find_element(By.NAME, "group_name").clear()
-        wd.find_element(By.NAME, "group_name").send_keys(group.name)
-        wd.find_element(By.NAME, "group_header").click()
-        wd.find_element(By.NAME, "group_header").clear()
-        wd.find_element(By.NAME, "group_header").send_keys(group.header)
-        wd.find_element(By.NAME, "group_footer").click()
-        wd.find_element(By.NAME, "group_footer").clear()
-        wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element(By.NAME, "submit").click()
-        self.return_to_group_page(wd)
-
-    def open_group_page(self, wd):
-        wd.find_element(By.LINK_TEXT, "groups").click()
-
-    def login(self, wd, username, password):
-        self.open_home_page(wd)
-        wd.find_element(By.NAME, "user").click()
-        wd.find_element(By.NAME, "user").clear()
-        wd.find_element(By.NAME, "user").send_keys(username)
-        wd.find_element(By.NAME, "pass").click()
-        wd.find_element(By.NAME, "pass").clear()
-        wd.find_element(By.NAME, "pass").send_keys(password)
-        wd.find_element(By.XPATH, "//input[@value='Login']").click()
-
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/")
-
-    def is_element_present(self, how, what):
-        try:
-            self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
-
-    def is_alert_present(self):
-        try:
-            self.wd.switch_to_alert()
-        except NoAlertPresentException as e:
-            return False
-        return True
-
-    def tearDown(self):
-        self.wd.quit()
-
+def test_add_empty_group(app):
+        app.login("admin", "secret")
+        app.create_group(Group(name="", header="", footer=""))
+        app.logout()
 
 if __name__ == "__main__":
     unittest.main()
