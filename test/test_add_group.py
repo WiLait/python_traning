abe465b7a9271cdc04230c8eba94c5072c8f6bab
WiLait <wilait.xaxa@gmail.com>
2025-03-17 12:11:15 +0600

    Puchenko

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,7 +2,6 @@
 
 from model.group import Group
 
-
 def test_add_group(app):
         app.session.login("admin", "secret")
         app.group.create(Group(name="sai", header="ydfghd", footer="dhryftd"))
